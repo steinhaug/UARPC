f094c5db8a40b54b353b2e86f025960ba0cbb3ea
Kim Steinhaug <kim@steinhaug.com>
2020-12-15 14:32:01 +0000

    DB Structure

---
+++ db.sql
@@ -0,0 +1,31 @@
+
+
+CREATE TABLE `userrights__userroles` (
+	`UserID` INT(10) NOT NULL,
+	`RoleID` INT(10) NOT NULL,
+	`AssignmentDate` INT(10) NOT NULL,
+	PRIMARY KEY (`UserID`, `RoleID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+CREATE TABLE IF NOT EXISTS `userrights__rolepermissions` (
+  `RoleID` int(11) NOT NULL,
+  `PermissionID` int(11) NOT NULL,
+  `AssignmentDate` int(11) NOT NULL,
+  PRIMARY KEY  (`RoleID`,`PermissionID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+CREATE TABLE IF NOT EXISTS `userrights__roles` (
+  `RoleID` int(11) NOT NULL auto_increment,
+  `title` varchar(128) NOT NULL,
+  `description` text NOT NULL,
+  PRIMARY KEY  (`RoleID`),
+  KEY `title` (`title`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+CREATE TABLE IF NOT EXISTS `userrights__permissions` (
+  `PermissionID` int(11) NOT NULL auto_increment,
+  `title` char(64) NOT NULL,
+  `description` text NOT NULL,
+  PRIMARY KEY  (`PermissionID`),
+  KEY `title` (`title`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1;
